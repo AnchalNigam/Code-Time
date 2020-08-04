870a661b37014227bf88211ce468f74c91010d87
Anchal Nigam <anchal.nigam@gradeup.co>
2020-08-04 14:11:32 +0530

    next element in on(n)

--- NextLargeElement.py
+++ NextLargeElement.py
@@ -1,23 +1,41 @@
 #code
 T = int(input())
-def nextLargeElement(arr):
-    nextLargeElemArr = []
-    for idx1 in range(len(arr)):
-        flag = False
-        for idx2 in range(idx1+1, len(arr)):
-            if arr[idx2] > arr[idx1]:
-                flag = True
-                nextLargeElemArr.append(str(arr[idx2]))
-                break
-        if flag == False:
-            nextLargeElemArr.append('-1')
-    return nextLargeElemArr
+nextLargeElemFinalArr = []
+
+# def nextLargeElement(arr):
+#     nextLargeElemArr = []
+#     for idx1 in range(len(arr)):
+#         flag = False
+#         for idx2 in range(idx1+1, len(arr)):
+#             if arr[idx2] > arr[idx1]:
+#                 flag = True
+#                 nextLargeElemArr.append(str(arr[idx2]))
+#                 break
+#         if flag == False:
+#             nextLargeElemArr.append('-1')
+#     return nextLargeElemArr
         
+def nextLargeElement(arr):
+    nextLargeElemArr = ['-1'] * len(arr)
+    stack = []
+    stack.append(0)
+    for idx in range(1, len(arr)):
+      poppedIdx = stack.pop()
+      while(arr[idx] > arr[poppedIdx]):
+        nextLargeElemArr[poppedIdx] = str(arr[idx])
+        if len(stack) == 0:
+          break
+        poppedIdx = stack.pop()
+      if arr[idx] < arr[poppedIdx]:
+        stack.append(poppedIdx)
+      stack.append(idx)
+    return nextLargeElemArr
+
+       
+
 for t in range(T):
     sizeOfArr = int(input())
     arr = list(map(int, input().split()))
-    print(arr)
-    nextLargeElemFinalArr = []
     nextLargeElemFinalArr.append(nextLargeElement(arr))
     
 for nextLargeElems in nextLargeElemFinalArr:
