56289c43dfc2fb9df011d54fde490c072897d037
Anchal Nigam <anchal.nigam@gradeup.co>
2020-09-19 00:55:42 +0530

    max sub array continued

--- kadanesAlgo.py
+++ kadanesAlgo.py
@@ -11,3 +11,16 @@ for i in range(int(T)):
   sizeArr = input()
   arr = list(map(int,input().split()))
   print(maxSubArray(arr))
+
+# other way
+def maxSubArray(self, nums: List[int]) -> int:
+  finalMaxSum = float('-inf')
+  summation = 0
+  for idx in range(len(nums)):
+      maxSum = max(summation + nums[idx], nums[idx])
+      if maxSum > finalMaxSum:
+          finalMaxSum = maxSum
+          summation = finalMaxSum
+      else:
+          summation = maxSum
+  return finalMaxSum
